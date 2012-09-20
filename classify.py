65d5298f6bdcdf4dcfaebbf7656ffb3e3e8fac51
Marat Zaynutdinov <tsundokum@gmail.com>
2012-09-20 23:08:02 +0400

    extracted common fuctionality into team.py

--- classify.py
+++ classify.py
@@ -4,93 +4,30 @@
 Features
 """
 from time import time
-import math
-import getpass
 
-import pandas
-import numpy as np
+from sklearn.cross_validation import StratifiedKFold
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 
-from sklearn.cross_validation import StratifiedKFold
 
-DATA = ''
-if getpass.getuser()=='marat':
-    DATA = '/home/marat/kaggle.com/stackoverflow-data/'
-
-def get_dataframe(filename):
-    dataframe = pandas.io.parsers.read_csv(filename)
-    return dataframe
+from team import *
 
 def split_dataframe(df):
     kf = StratifiedKFold(df["OpenStatus"].values,5)
     train, test = kf.__iter__().next()
     return df.take(train), df.take(test)
 
-def save_dataframe(df, filename):
-    pass
-
-
-input_features = [
-    "PostId",
-    "PostCreationDate",
-    "OwnerUserId",
-    "OwnerCreationDate",
-    "ReputationAtPostCreation",
-    "OwnerUndeletedAnswerCountAtPostTime",
-    "Title",
-    "BodyMarkdown",
-    "Tag1",
-    "Tag2",
-    "Tag3",
-    "Tag4",
-    "Tag5",
-    "PostClosedDate",
-    "OpenStatus",
-]
-
-statuses = {
-    "open": 0,
-    "not a real question": 1,
-    "not constructive": 2,
-    "off topic": 3,
-    "too localized": 4
-}
-
-
-def status_to_number(status):
-    return statuses[status]
-
-def extract_features(features, df):
-    ff = pandas.DataFrame(index=df.index)
-    for feature in features:
-        if feature in df:
-            if feature == "OpenStatus":
-                ff = ff.join(df[feature].apply(status_to_number))
-            else:
-                ff = ff.join(df[feature])
-    return ff
-
-
-def mcll(predicted, actual):
-    '''Calculate MCLL
-    
-    predicted -- numpy array(NxM) of probabilites where N -- num of obs and M is number of classes
-    
-    actual -- iterable integer 1-d array of actual classes'''      
-    predicted = predicted/predicted.sum(1)[:,np.newaxis] #normalize
-    return -np.sum(np.log(predicted[np.arange(predicted.shape[0]),actual]))/predicted.shape[0]
-
-def calculate_mcll(probs, observations):
-    mcll = 0.0
-    for i, observation in enumerate(observations):
-        mcll = mcll + math.log(probs[i][observation - 1])
-    mcll = - mcll / len(observations)
-    return mcll
+#def calculate_mcll(probs, observations):
+#    mcll = 0.0
+#    for i, observation in enumerate(observations):
+#        mcll = mcll + math.log(probs[i][observation - 1])
+#    mcll = - mcll / len(observations)
+#    return mcll
 
 
 #although it is recommended to put everything into main() 
@@ -100,7 +37,7 @@ def calculate_mcll(probs, observations):
 
 print("Reading data...")
 
-test_df = get_dataframe(DATA + "./train-sample.csv")
+test_df = get_dataframe(DATA_PATH + "./train-sample.csv")
 test_ff = extract_features(input_features, test_df)
 
 # split ~140k into ~100k training and ~40k test
---
+++ team.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Some common functions and variables
+
+Created on Thu Sep 20 23:00:26 2012
+"""
+
+import getpass
+import pandas
+
+import numpy as np
+
+DATA_PATH = ''
+if getpass.getuser()=='marat':
+    DATA_PATH = '/home/marat/kaggle.com/stackoverflow-data/'
+
+input_features = [
+    "PostId",
+    "PostCreationDate",
+    "OwnerUserId",
+    "OwnerCreationDate",
+    "ReputationAtPostCreation",
+    "OwnerUndeletedAnswerCountAtPostTime",
+    "Title",
+    "BodyMarkdown",
+    "Tag1",
+    "Tag2",
+    "Tag3",
+    "Tag4",
+    "Tag5",
+    "PostClosedDate",
+    "OpenStatus",
+]
+
+statuses = {
+    "open": 0,
+    "not a real question": 1,
+    "not constructive": 2,
+    "off topic": 3,
+    "too localized": 4
+}    
+    
+def get_dataframe(filename):
+    dataframe = pandas.io.parsers.read_csv(filename)
+    return dataframe    
+    
+
+def status_to_number(status):
+    return statuses[status]
+
+def extract_features(features, df):
+    ff = pandas.DataFrame(index=df.index)
+    for feature in features:
+        if feature in df:
+            if feature == "OpenStatus":
+                ff = ff.join(df[feature].apply(status_to_number))
+            else:
+                ff = ff.join(df[feature])
+    return ff
+
+def mcll(predicted, actual):
+    '''Calculate MCLL
+    
+    predicted -- numpy array(NxM) of probabilites where N -- num of obs and M is number of classes
+    
+    actual -- iterable integer 1-d array of actual classes'''      
+    predicted = predicted/predicted.sum(1)[:,np.newaxis] #normalize
+    return -np.sum(np.log(predicted[np.arange(predicted.shape[0]),actual]))/predicted.shape[0]
