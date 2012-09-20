b6984ceefbf903624fbcc99de407b2b5937e0aa3
marat <marat@marat.(none)>
2012-09-20 22:46:08 +0400

    lots of refactoring

--- classify.py
+++ classify.py
@@ -1,28 +1,35 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Features
 """
-import time
+from time import time
 import math
+import getpass
 
 import pandas
 import numpy as np
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.svm.sparse import LinearSVC
+from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 
+from sklearn.cross_validation import StratifiedKFold
+
+DATA = ''
+if getpass.getuser()=='marat':
+    DATA = '/home/marat/kaggle.com/stackoverflow-data/'
 
 def get_dataframe(filename):
     dataframe = pandas.io.parsers.read_csv(filename)
     return dataframe
 
-
-def split_dataframe(df, threshold=0.7):
-    threshold_index = len(df) * threshold
-    return df[:threshold_index], df[threshold_index:]
-
+def split_dataframe(df):
+    kf = StratifiedKFold(df["OpenStatus"].values,5)
+    train, test = kf.__iter__().next()
+    return df.take(train), df.take(test)
 
 def save_dataframe(df, filename):
     pass
@@ -47,36 +54,36 @@ input_features = [
 ]
 
 statuses = {
-    "open": 1,
-    "not a real question": 2,
-    "not constructive": 3,
-    "off topic": 4,
-    "too localized": 5
+    "open": 0,
+    "not a real question": 1,
+    "not constructive": 2,
+    "off topic": 3,
+    "too localized": 4
 }
 
 
-def reduce_number_of_classes(status):
+def status_to_number(status):
     return statuses[status]
-
 
 def extract_features(features, df):
     ff = pandas.DataFrame(index=df.index)
     for feature in features:
         if feature in df:
             if feature == "OpenStatus":
-                ff = ff.join(df[feature].apply(reduce_number_of_classes))
+                ff = ff.join(df[feature].apply(status_to_number))
             else:
                 ff = ff.join(df[feature])
     return ff
 
 
-def filter_opened_questions(df):
-    return df[df["OpenStatus"] == "open"]
-
-
-def filter_closed_questions(df):
-    return df[df["OpenStatus"] != "open"]
-
+def mcll(predicted, actual):
+    '''Calculate MCLL
+    
+    predicted -- numpy array(NxM) of probabilites where N -- num of obs and M is number of classes
+    
+    actual -- iterable integer 1-d array of actual classes'''      
+    predicted = predicted/predicted.sum(1)[:,np.newaxis] #normalize
+    return -np.sum(np.log(predicted[np.arange(predicted.shape[0]),actual]))/predicted.shape[0]
 
 def calculate_mcll(probs, observations):
     mcll = 0.0
@@ -86,36 +93,43 @@ def calculate_mcll(probs, observations):
     return mcll
 
 
-if __name__ == "__main__":
+#although it is recommended to put everything into main() 
+# I prefer dirty way. Lots of benefits when using interactive
+# IDE such as IPython
+#if __name__ == "__main__":
 
-    print("Reading data...")
+print("Reading data...")
 
-    test_df = get_dataframe("./train-sample.csv")
-    test_ff = extract_features(input_features, test_df)
+test_df = get_dataframe(DATA + "./train-sample.csv")
+test_ff = extract_features(input_features, test_df)
 
-    # split ~140k into ~100k training and ~40k test
-    ff_train, ff_val = split_dataframe(test_ff)
+# split ~140k into ~100k training and ~40k test
+ff_train, ff_val = split_dataframe(test_ff)
 
-    print("Training...")
+print("Training...")
 
-    t1 = time.time()
+t1 = time()
 
-    vectorizer = CountVectorizer()
-    train_counts = vectorizer.fit_transform(ff_train["BodyMarkdown"])
-    tfidf_transformer = TfidfTransformer()
+vectorizer = CountVectorizer()
+train_counts = vectorizer.fit_transform(ff_train["BodyMarkdown"])
+tfidf_transformer = TfidfTransformer()
 
-    # 98190x285052
-    train_tfidf_table = tfidf_transformer.fit_transform(train_counts)
+# 98190x285052
+train_tfidf_table = tfidf_transformer.fit_transform(train_counts)
 
-    clf = LinearSVC().fit(train_tfidf_table, ff_train["OpenStatus"])
+clf = LinearSVC().fit(train_tfidf_table, ff_train["OpenStatus"])
 
-    print("Testing...")
+print("Testing...")
 
-    test_counts = vectorizer.transform(ff_val["BodyMarkdown"])
-    test_tfidf_table = tfidf_transformer.transform(test_counts)
+test_counts = vectorizer.transform(ff_val["BodyMarkdown"])
+test_tfidf_table = tfidf_transformer.transform(test_counts)
 
-    predict = clf.predict(test_tfidf_table)
-    print("np.mean: %f" % (np.mean(predict == ff_val["OpenStatus"])))
+predict = clf.predict(test_tfidf_table)
+print("np.mean: %f" % (np.mean(predict == ff_val["OpenStatus"])))
 
-    t2 = time.time()
-    print("done in %d seconds" % (t2 - t1))
+linear_decisions = clf.decision_function(test_tfidf_table)
+predictedProbs = (1/(1+np.exp(-linear_decisions)))**3.5
+print("MLCC: %f" %(mcll(predictedProbs, ff_val["OpenStatus"].values)))
+
+t2 = time()
+print("done in %d seconds" % (t2 - t1))
