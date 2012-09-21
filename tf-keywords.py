e54605bfe40c8899605b9d6c956e027620e8b6cc
Marat Zaynutdinov <tsundokum@gmail.com>
2012-09-21 19:49:27 +0400

    bug fix + dump result to a file

--- tf-keywords.py
+++ tf-keywords.py
@@ -6,9 +6,9 @@ Created on Thu Sep 20 22:51:35 2012
 
 from common import *
 import pandas
+import cPickle
 
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 
 data_raw = get_dataframe(DATA_PATH + "./train-sample.csv")
 data = extract_features(['Tag%d' %i for i in range(1,6)], data_raw)
@@ -20,31 +20,7 @@ def convert_to_string(row):
 
 keywords_rows = data.apply( convert_to_string , axis=1 )
 
-#keywords_rows.to_csv(DATA_PATH + "/keywords.csv", header=False, index=False, mode='wb')
+vectorizer = CountVectorizer(stop_words=None,analyzer = 'word',token_pattern='[^\s]+')
+counts = vectorizer.fit_transform(keywords_rows.values)
 
-#vectorizer = CountVectorizer(stop_words=None)
-#train_counts = vectorizer.fit_transform(keywords_rows.values)
-#
-#tfidf_transformer = TfidfTransformer(norm=None, use_idf=False)
-#train_tfidf_table = tfidf_transformer.fit_transform(train_counts)
-#
-#
-#clf = LinearSVC().fit(train_tfidf_table, ff_train["OpenStatus"])
-#
-#print("Testing...")
-#
-#test_counts = vectorizer.transform(ff_val["BodyMarkdown"])
-#test_tfidf_table = tfidf_transformer.transform(test_counts)
-#
-#predict = clf.predict(test_tfidf_table)
-#print("np.mean: %f" % (np.mean(predict == ff_val["OpenStatus"])))
-#
-##it is not very different from Uniform Benchmark =))
-#linear_decisions = clf.decision_function(test_tfidf_table)
-#predictedProbs = (1/(1+np.exp(-linear_decisions)))**3.5
-#print("MLCC: %f" %(mcll(predictedProbs, ff_val["OpenStatus"].values)))
-#
-#t2 = time()
-#print("done in %d seconds" % (t2 - t1))
-#
-
+cPickle.dump(counts, open(DATA_PATH+"small_tables/keyword_tf.sparse","wb"), protocol=-1)
